3568bb62e1ad1b40bcbaa1ee3b48afd8312ee7bc
BubacarrC <buba.conteh@utg.edu.gm>
2016-12-11 14:43:18 +0100

    Update app.js

--- nodejs_app/app.js
+++ nodejs_app/app.js
@@ -1,5 +1,5 @@
 // Load the http module to create an http server.
-var http = require('http');
+var http = require123('http');
 
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
