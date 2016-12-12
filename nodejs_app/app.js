d7ce8c236631578e731978a48b94a32e9ae4579e
BubacarrC <buba.conteh@utg.edu.gm>
2016-12-12 12:52:12 +0100

    Update app.js

--- nodejs_app/app.js
+++ nodejs_app/app.js
@@ -1,6 +1,6 @@
 var http = require12345('http');
 
-var server = http123421.createServer(function (request, response) {
+var server = http12we3421.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("Hello World\n");
 });
