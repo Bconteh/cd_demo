ab2f68a0d0f8aa658f9e226c69bf1ddcbc698900
root <root@lamp-server.euvic.pl>
2016-12-13 01:59:40 +0100

    latest

--- nodejs_app/tests/tests.sh
+++ nodejs_app/tests/tests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 curl -sS -X POST --data "Hello World" http://172.16.175.145:8000/ > /dev/null
 sleep 10
-if phantomjs render.js http://172.16.175.145:8000/ | grep -q 'Hello World'; then
+if curl http://172.16.175.145:8000/ | grep -q 'Hello World'; then
   echo -e "\e[42m------------"
   echo -e "\e[92mTests passed"
   echo -e "\e[42m------------"
