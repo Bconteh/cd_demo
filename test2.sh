b7f0db735af12238890d5a05eb99010509394daf
BubacarrC <buba.conteh@utg.edu.gm>
2016-12-13 22:01:35 +0100

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -2,3 +2,6 @@ echo "I'm a test script. I will be able print this message for you on the consol
 echo "I'm a test script. I will be able print this message for you on the console";
 echo "I'm a test script. I will be able print this message for you on the console";
 echo "I'm a test script. I will be able print this message for you on the console";
+echo "I'm a test script. I will be able print this message for you on the console";
+echo "I'm a test script. I will be able print this message for you on the console";
+echo "I'm a test script. I will be able print this message for you on the console";
