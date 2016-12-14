71b8f23db7644c747075ea617e435758c9418eb0
BubacarrC <buba.conteh@utg.edu.gm>
2016-12-14 21:06:08 +0100

    Update testsA.js

--- nodejs_app/testsA.js
+++ nodejs_app/testsA.js
@@ -1,6 +1,6 @@
 var t = 0;
 const exec = require('child_process').exec;
-exec('node nodejs_app/app.js', (error, stdout, stderr)=>{
+exec('node nodejs_app/app.js', function(error, stdout, stderr){
   if (error) {
     console.error('exec error: ${error}');
 	t =1;
@@ -9,5 +9,13 @@ exec('node nodejs_app/app.js', (error, stdout, stderr)=>{
   console.log('stdout: ${stdout}');
   console.log('stderr: ${stderr}');
 });
-exec('killall node'); 
+exec('killall node',function(error, stdout, stderr){
+  if (error) {
+    console.error('exec error: ${error}');
+	t =1;
+    return;
+  }
+  console.log('stdout: ${stdout}');
+  console.log('stderr: ${stderr}');
+}); 
 return t;
