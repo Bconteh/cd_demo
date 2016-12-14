f0ecd03d4adb167821d7dca628774ff60f0c99ba
BubacarrC <buba.conteh@utg.edu.gm>
2016-12-14 20:04:56 +0100

    Update testsA.js

--- nodejs_app/testsA.js
+++ nodejs_app/testsA.js
@@ -1,14 +1,22 @@
 var exec= require('child_process').exec;
 var tt;
-var cmd ='./nodejs_app/nodeon.sh ; tt=$!' ;
+var cmd ='./nodejs_app/nodeon.sh' ;
 
-var t=exec('whoami',function (error,stdout, stderr){
+var t=exec(cmd,function (error,stdout, stderr){
 	tt=stdout.replace('\r\n','');
+	if(error !== null){
+		console.log('Error: '+error);
+		tt = 1;
+	}
+	else{
+		tt = 0;
+		console.log('Succeeded: Thanks');
+	}
 });
 
 exec("./nodejs_app/nodeoff.sh");
 console.log(tt);
 if(tt=0)
-return 1;
-else return 0;
+return 0;
+else return 1;
 
