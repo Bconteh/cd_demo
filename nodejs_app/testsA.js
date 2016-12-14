4b31dfb95be5aad6dd425a4fd57ba7134b1e6fd2
BubacarrC <buba.conteh@utg.edu.gm>
2016-12-14 21:09:10 +0100

    Update testsA.js

--- nodejs_app/testsA.js
+++ nodejs_app/testsA.js
@@ -4,10 +4,14 @@ exec('node nodejs_app/app.js', function(error, stdout, stderr){
   if (error) {
     console.error('exec error: ${error}');
 	t =1;
-    return;
+    return 1;
   }
+ else{
+	 
   console.log('stdout: ${stdout}');
   console.log('stderr: ${stderr}');
+  return 0;
+ }
 });
 exec('killall node',function(error, stdout, stderr){
   if (error) {
