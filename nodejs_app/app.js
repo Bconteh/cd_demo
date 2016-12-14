9932ab8156ed9ad10bcc371a32e0c7f8109236bd
BubacarrC <buba.conteh@utg.edu.gm>
2016-12-14 15:12:24 +0100

    Update app.js

--- nodejs_app/app.js
+++ nodejs_app/app.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-var server = http.createServer(function (request, response) {
+var server = http.createServer(function (request, response) {m,hkjhklhjknjk
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("Hello World\n");
 });
