f30bb4819a03d1cc002f84fd1b7ace4bc32b1250
BubacarrC <buba.conteh@utg.edu.gm>
2016-12-15 03:32:15 +0100

    Update app.js

--- nodejs_app/app.js
+++ nodejs_app/app.js
@@ -1,10 +1,10 @@
 var http = require('http');
 
-var server = http12345.createServer(function (request, response) {
+var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("Hello World\n"); 
 });
 
-server.listen(8000);aganze
+server.listen(8000);
 
 console.log("Server is running at http://127.0.0.1:8000/");
