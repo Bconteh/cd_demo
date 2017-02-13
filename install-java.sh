fa745bcd62b5f01756ad947320885ae1cfe03ce8
BubacarrC <buba.conteh@utg.edu.gm>
2017-02-13 11:18:24 +0100

    Create install-java.sh

---
+++ install-java.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# From http://www.webupd8.org/2014/03/how-to-install-oracle-java-8-in-debian.html
+#
+# You need to run this script as root
+#
+
+# su -
+echo "INSTALLING JAVA 8 AS USER `whoami` "
+
+echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee /etc/apt/sources.list.d/webupd8team-java.list
+echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
+
+# Accept license non-iteractive
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
+apt-get update
+apt-get install -y oracle-java8-installer
+
+# Make sure Java 8 becomes default java
+apt-get install -y oracle-java8-set-default
+
+# exit
